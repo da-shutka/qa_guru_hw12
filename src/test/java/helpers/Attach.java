665738132aff45580407b3bb36f716bc1865d58d
Daria Petrova <petrova.tpu@gmail.com>
2024-05-30 22:55:11 +0300

    HW-12: Fix for video path

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -49,7 +49,7 @@ public class Attach {
     public static URL getVideoUrl() {
         //String wdhost = System.getProperty("wdhost", "");
         //String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
-        String videoUrl = wdHost + "/video" + sessionId() + ".mp4";
+        String videoUrl = wdHost + "/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
